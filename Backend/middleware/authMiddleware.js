abaf27ea1869800116192ea707cceafce5dd24ed
Anand <hyeiamaka06@gmail.com>
2023-09-13 21:50:49 +0530

    error handling improved

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,17 +1,22 @@
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
+const { asyncHandler } = require('./asyncHandler')
 dotenv.config()
 const secret = process.env.JWT_KEY
 
-exports.authMiddleWare = async (req, res, next) => {
-  try {
-    const token = req.headers.Authorization.split(' ')[1]
-    console.log(token)
-    if (token) {
-      const decoded = jwt.verify(token, secret)
-      console.log(decoded)
-      req.body._id = decoded?.id
-    }
+exports.authMiddleWare = asyncHandler(async (req, res, next) => {
+  if(req.headers.Authorization){
+  const token = req.headers.Authorization.split(' ')[1]
+  console.log(token)
+  if (token) {
+    const decoded = jwt.verify(token, secret)
+    console.log(decoded)
+    req.body._id = decoded?.id
     next()
-  } catch (error) {}
+  } else {
+    next(new Error('You are not authorized for this request!!', 400))
+  }
+}else{
+   next(new Error('You are not logged In', 400))
 }
+})
